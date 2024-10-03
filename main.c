ee51eed2a6a7a4bf4f0bba642839b7416c7ece3f
LuckyMG1 <luckymg751@gmail.com>
2024-10-03 15:47:40 +0200

    Added main.c skeleton code

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+float A;
+float B;
+float C;
+
+float calculateX() {
+    return 0;
+}
+
+float calculateY() {
+    return 0;
+}
+
+float calculateZ() { return 0;  }
+int main() {
+    return 0;
+}
